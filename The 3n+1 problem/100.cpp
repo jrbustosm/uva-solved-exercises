a16e87b41ba5c4bf7bf35a8995a5636ef2f74c3e
jrbustosm <jrbustosm@gmail.com>
2012-07-25 14:56:33 -0500

    Improving 100.cpp, comments

--- The 3n+1 problem/100.cpp
+++ The 3n+1 problem/100.cpp
@@ -1,18 +1,28 @@
-// Jose Ricardo Bustos Molina
+/* 
+
+the 3n+1 problem
+
+Jose Ricardo Bustos Molina
+
+*/
 
 #include <iostream>
 
+//Tamaño máximo del cache
 #define MAXS 30000001
 
 using namespace std;
 
+//Maximim size of chache
 static int longs[MAXS];
-long encontrarlong(long);
+
+long findlong(long);
 
 int main ()
 {
   long i, j;
 
+  //cache initialization
   for(long k=0; k<MAXS; ++k) longs[k] = 0;
 
   while(cin >> i >> j){
@@ -25,11 +35,12 @@ int main ()
       f = i;
     }
 
-    long max = encontrarlong(o);
+    //Assume that the maximum cycle length is left value of interval
+    long max = findlong(o);
     for(long k=o+1; k<=f; ++k){
-      long c = encontrarlong(k);
+      long c = findlong(k);
       if(c>max){
-        max = c;
+        max = c; //if find one higher, then replace
       }
     }
     cout << i << " " << j << " " << max << endl;
@@ -37,13 +48,15 @@ int main ()
   return 0;
 }
 
-long encontrarlong(long n){
+//this function find the cycle length, using the algoritgm 3n+1 
+long findlong(long n){
   if(n<MAXS) if(longs[n] != 0) return longs[n];
   if(n==1) return 1;
   long nn = n/2;
   if(n & 1)
     nn = 3*n+1;
-  long res = encontrarlong(nn)+1;
+  long res = findlong(nn)+1;
   if(n<MAXS) longs[n] = res;
   return res;
 }
+
