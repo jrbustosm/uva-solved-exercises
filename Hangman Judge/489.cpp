84f1c3aeced84975c85b3e1c7e61cc26c3743253
jrbustosm <jrbustosm@gmail.com>
2012-07-27 13:30:07 -0500

    upload Hangman judge problem

---
+++ Hangman Judge/489.cpp
@@ -0,0 +1,52 @@
+/* 
+
+the 3n+1 problem
+
+Jose Ricardo Bustos Molina
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  int r;
+  cin >> r;
+  cin.get();
+  while(r!=-1){
+    string w, s;
+    bool v[255];
+    for(int i=0; i<255; ++i) v[i] = false;
+    getline(cin,w);
+
+    int nl = 0;
+    for(string::iterator it=w.begin(); it < w.end(); ++it){
+      if(!v[(int)*it]) ++nl;
+      v[(int)*it] = true;
+    }
+
+    getline(cin,s);
+    int error = 0, acierto=0;
+    for(string::iterator it=s.begin(); it < s.end(); ++it){
+      if(v[(int)*it]){
+        ++acierto;
+        if(nl==acierto) break;
+        v[(int)*it] = false;
+      }
+      else {
+        ++error;
+        if(error>=7) break;
+      }
+    }
+
+    cout << "Round " << r << endl;    
+    if(nl==acierto && error<7) cout << "You win." << endl;
+    else if(error<7) cout << "You chickened out." << endl;
+    else cout << "You lose." << endl;
+
+    cin >> r;
+    cin.get();
+  }
+  return 0;
+}
