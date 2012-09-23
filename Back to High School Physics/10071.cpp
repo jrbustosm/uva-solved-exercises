f0a3c80e5cf277fd8e3e65364ce87d970ff27cb4
jrbustosm <jrbustosm@gmail.com>
2012-09-23 16:43:26 -0500

    upload Back to High School Physics problem

---
+++ Back to High School Physics/10071.cpp
@@ -0,0 +1,20 @@
+/* 
+
+Back to High School Physics
+
+Jose Ricardo Bustos Molina
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+  int v, t;
+  while(cin >> v >> t){
+    cout << v*t*2 << endl;
+  }
+  return 0;
+}
