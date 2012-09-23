4e7958136b6a03985d2822e69c4b602c9a0f0141
jrbustosm <jrbustosm@gmail.com>
2012-09-23 15:13:20 -0500

    upload The snail problem

---
+++ The Snail/573.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  double H, U, D, F;
+  cin >> H >> U >> D >> F;
+  while(H!=0){
+    int day = 1;
+    double h = 0;
+    while(true){
+      double delta = F*(day-1);
+      if(delta > 100) delta = 100;
+      h += U*(100.0-delta)/100.0;
+      if(h > H){
+        cout << "success on day " << day << endl;
+        break;
+      }
+      h -= D;
+      if(h < 0){
+        cout << "failure on day " << day << endl;
+        break;
+      }
+      ++day;
+    }
+    cin >> H >> U >> D >> F;
+  }
+  return 0;
+}
