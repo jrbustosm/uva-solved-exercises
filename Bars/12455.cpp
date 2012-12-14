3b85e401fa6147817c5c3064b6f5f0145dac1d2e
jrbustosm <jrbustosm@gmail.com>
2012-12-14 12:20:14 -0500

    mejora para velocidad, no funciono :(

--- Bars/12455.cpp
+++ Bars/12455.cpp
@@ -6,25 +6,24 @@ Jose Ricardo Bustos Molina
 
 */
 
-#include <iostream>
+#include <cstdio>
 
 using namespace std;
 
-bool solder(int *v, int n, int z, int sum, int &L);
+bool solder(int *v, int &n, int z, int sum, int &L);
 
 int main(){
-  int T;
-  cin >> T;
+  int v[20];
+  int T, K, N, i;
+  scanf("%d", &T);
   while(T--){
-    int K, N;
-    cin >> K >> N;
-    int* v = new int[N];
-    for(int i=0; i<N; ++i) cin >> v[i];
-    cout << (solder(v, N, 0, 0, K)?"YES":"NO") << endl;
+    scanf("%d %d", &K, &N);
+    for(i=0; i<N; ++i) scanf("%d",&v[i]);
+    puts(solder(v, N, 0, 0, K)?"YES":"NO");
   }
 }
 
-bool solder(int *v, int n, int z, int sum, int &L){
+bool solder(int *v, int &n, int z, int sum, int &L){
   if(sum == L) return true;
   if(sum > L) return false;
   for(int i=z; i<n; ++i) if(solder(v, n, i+1, sum+v[i], L)) return true;
